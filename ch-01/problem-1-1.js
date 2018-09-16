bf2e890761d5565a7575c34ff826264c75378d9e
Matthew Gehring <mgehring@ucmerced.edu>
2018-09-16 12:02:47 -0700

    add converted seconds to miliseconds function

---
+++ ch-01/problem-1-1.js
@@ -0,0 +1,53 @@
+var time = [
+  {unit : "second", value: 1},
+  {unit : "minute", value: 1},
+  {unit : "hour", value: 1},
+  {unit : "day", value: 1},
+  {unit : "month", value: 1},
+  {unit : "year", value: 1},
+  {unit : "century", value: 1}
+];
+
+function selectUnitMultiplier(unit){
+  var multiplier = 0;
+  var newUnit = "Foo";
+  if(unit == "second"){
+    multiplier = 1000;
+    newUnit = "milisecond";
+  }else if (unit == "minute") {
+    multiplier = 60;
+    newUnit = "second";
+  }else if (unit == "hour") {
+    multiplier = 60;
+    newUnit = "minute";
+  }else if (unit == "day") {
+    multiplier = 24;
+    newUnit = "hour";
+  }else if (unit == "month") {
+    multiplier = 31;
+    newUnit = "day";
+  }else if (unit == "year") {
+    multiplier = 365;
+    newUnit = "day";
+  }else if (unit == "century") {
+    multiplier = 100;
+    newUnit = "year";
+  }
+  return [multiplier, newUnit];
+}
+
+function convertToMiliseconds(time){
+  var ms = 0;
+  var update = [];
+  while (!(time.unit == "milisecond")){
+    update = selectUnitMultiplier(time.unit);
+    time.value *= update[0];
+    time.unit = update[1];
+  };
+}
+
+console.log(time);
+time.forEach(function(time){
+  convertToMiliseconds(time);
+});
+console.log(time);
